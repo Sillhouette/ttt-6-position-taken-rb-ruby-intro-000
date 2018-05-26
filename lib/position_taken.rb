b89812c0012e9235dfbe93071dc03cfdd2a49276
Richard Melchior <raustin.melchior@gmail.com>
2018-05-26 20:08:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if(board[index] == " " || board[index] == "" || board[index] == nil)
     return false
-  else#if(board[index] == "X" || board[index] == ["O"])
+  else
     return true
   end
 end
